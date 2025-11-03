a101f55c63967c2ac25097246ccfd58027db9b74
Maalik-Gen <maalik.gen@gmail.com>
2025-11-03 10:13:19 -0500

    Formatting changes

--- Image_Search_App.py
+++ Image_Search_App.py
@@ -11,7 +11,7 @@ from pathlib import Path
 
 # Adding comments to this portion to see if the alert system works
 
-# Testing alert system with changes
+# Testing alert system for formatting changes
 
 # Alert System is working!!!
 
